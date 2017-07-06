d9460beb536831b5a61a6b76b3abfcf664696e78
瞬联软件-朱艳雯 <K0240001@test.htsc.com.cn>
2017-07-06 14:54:26 +0800

    菜单+tab方式显示

---
+++ src/config/tabConfig.js
@@ -0,0 +1,35 @@
+/**
+ * * @file config/tabConfig.js
+ *  tab菜单栏设置
+ * @author zhuyanwen
+ */
+const tabConfig = {
+  example: {
+    key: '1',
+    value: 'example',
+    title: 'Home',
+    url: '/example',
+  },
+
+  menu1: {
+    key: '2',
+    value: 'menu1',
+    title: '菜单一',
+    url: '/menu1',
+
+  },
+  menu21: {
+    key: '3',
+    value: 'menu21',
+    title: '子菜单一',
+    url: '/menu2/menu21',
+  },
+  menu22: {
+    key: '4',
+    value: 'menu22',
+    title: '子菜单2',
+    url: '/Menu2/menu22',
+  },
+
+};
+export default tabConfig;
